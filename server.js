637d01119011af39d0fffd0d26e5bf43d5e6d45d
P1zza-L1cht <haruki21corder@gmail.com>
2022-05-24 16:03:39 +0900

    serverSetting

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 const express = require('express');
+const app = express();
+
+const port = process.env.PORT||5000;
+
+app.get('/api/profile', (req, res)=> {
+  const contents = [
+    {id: 1, express: "send data to the react",
+     id: 2, react: "get data from express"}
+  ];
+  res.json(contents);
+});
+
+app.listen(port, () => {console.log(`Listning to the Server on port ${port}`)});
