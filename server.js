935742054751e48407164ef5afd3923735c99190
P1zza-L1cht <haruki21corder@gmail.com>
2022-05-25 23:28:49 +0900

    save

--- server.js
+++ server.js
@@ -4,14 +4,11 @@ const app = express();
 const port = process.env.PORT||5000;
 
 app.get('/api/profile', (req, res)=> {
-  const contents = connect.query(
-    'SELECT * FROM users',
-    (error, results) => {
-      console.log(results);
-      return results;
-    }
-  );
-  res.json(contents);
+  const skills = [
+    {id: 1, name: 'HTML'},
+    {id: 2, name: 'javascript'}
+  ];
+  res.json(skills);
 });
 
 app.listen(port, () => {console.log(`Listening to the Server on port ${port}`)});
