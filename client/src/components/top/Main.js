00dd72b857fa802716b96a1822de3038676241c5
P1zza-L1cht <haruki21corder@gmail.com>
2022-05-27 01:38:40 +0900

    top

--- client/src/components/top/Main.js
+++ client/src/components/top/Main.js
@@ -9,45 +9,27 @@ import Contact from './partTop/Contact';
 class Main extends React.Component {
   constructor() {
     super();
-    this.state = {contentNum: 1};
+    this.myRef = React.createRef();
+    this.state = {
+      howScrolled: 0,
+    };
   }
 
-  setContentNum = (n) =>  {
-    this.setState({contentNum: n});
-  }
+  handleScroll = () => {
+    this.setState({
+      howScrolled: this.myRef.scrollTop
+    });
+  };
 
   render() {
-    let content;
-    // eslint-disable-next-line default-case
-    switch(this.state.contentNum) {
-      case 1:
-        content = <MainVisual />;
-        break;
-      case 2:
-        content = <About />;
-        break;
-      case 3:
-        content = <Skills />;
-        break;
-      case 4:
-        content = <Product />;
-        break;
-      case 5:
-        content = <Contact />;
-        break;
-    }
     return (
-      <div id="main">
-        {content}
-        <div id='switch'>
-          <ul>
-            <li><div className='circle-btn' onClick={() => {this.setContentNum(1)}}></div></li>
-            <li><div className='circle-btn' onClick={() => {this.setContentNum(2)}}></div></li>
-            <li><div className='circle-btn' onClick={() => {this.setContentNum(3)}}></div></li>
-            <li><div className='circle-btn' onClick={() => {this.setContentNum(4)}}></div></li>
-            <li><div className='circle-btn' onClick={() => {this.setContentNum(5)}}></div></li>
-          </ul>
-        </div>
+      <div id="main"  onScroll={()=>{this.handleScroll()}} ref={this.myRef}>
+        <div id='bg-cover'></div>
+        <MainVisual/>
+        <About/>
+        <Skills/>
+        <Product/>
+        <Contact/>
       </div>
     );
   }
--- client/src/components/top/partTop/MainVisual.js
+++ client/src/components/top/partTop/MainVisual.js
@@ -4,6 +4,7 @@ class MainVisual extends React.Component {
   render() {
     return (
       <div className="main-visual">
+        <div className='visual-image'></div>
       </div>
     );
   }
